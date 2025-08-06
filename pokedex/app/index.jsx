6aed4725ddf3193cdbcc4882419f742e01caa845
nicoly-athayde <nicolyathayde4@gmail.com>
2025-08-06 13:59:58 -0300

    Continuação

---
+++ pokedex/app/index.jsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+import Header from '@/components/Header';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+        <Header title="Pokédex" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    flexDirection: 'column',
+  },
+  cards: {
+    padding: 15,
+  },
+});

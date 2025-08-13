8237e245b5b22495524122842e1fab37a537c501
nicoly-athayde <nicolyathayde4@gmail.com>
2025-08-13 14:02:37 -0300

    Af

--- pokedex/app/index.jsx
+++ pokedex/app/index.jsx
@@ -1,10 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, Flatlist, StyleSheet, Text, View } from "react-native";
+import { pokemons } from "@/data/pokemons"
 import Header from '@/components/Header';
+import Card from "../components/Card";
 
 export default function Index() {
+  
+  const renderPokemon = ({ item }) => (
+    <Card pokemon={item}/>
+  )
+  
   return (
     <View style={styles.container}>
         <Header title="Pokédex" />
+        <FlatList
+          data={pokemons}
+          style={styles.cards}
+          keyExtractor={(pokemon) => pokemon.Numero.toString()}
+          renderItem={ renderPokemon }
+          initialNumToRender={10}
+          maxToRenderPerBatch={10}
+          windowSize={5}
+        />
     </View>
   );
 }
